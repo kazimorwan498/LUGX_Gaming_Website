7cefd987cfc2892c769bbd07543d81bb7cd0eedc
kazimorwan498 <kazimdmorwan498@gmail.com>
2024-10-27 18:56:48 +0600

    added header

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/*"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "rgb(0,113,248)",
+                secondary: "rgb(238,98,107)",
+                white: "rgb(255, 255, 255)",
+                black: "rgb(0, 0, 0)",
+            },
+        },
+        colors: {
+            primary: "rgb(0,113,248)",
+            secondary: "rgb(238,98,107)",
+            white: "rgb(255, 255, 255)",
+            black: "rgb(0, 0, 0)",
+        },
+    },
+    plugins: [],
+};
