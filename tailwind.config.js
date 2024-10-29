1f3bd62a754c90602a95d68eac5dcdf7ce820358
kazimorwan498 <kazimdmorwan498@gmail.com>
2024-10-29 21:01:59 +0600

    customize shop & newsletter section

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
             secondary: "rgb(238,98,107)",
             white: "rgb(255, 255, 255)",
             black: "rgb(30, 30, 30)",
+            lightGray: "rgb(232, 232, 232)",
         },
     },
     plugins: [],
