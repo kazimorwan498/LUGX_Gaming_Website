f24ff127f028d3b9f69f4974fe07d84417f74f14
kazimorwan498 <kazimdmorwan498@gmail.com>
2024-11-05 18:18:09 +0600

    solve url problem

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "/LUGX_Gaming_Website/",
     build: {
         rollupOptions: {
             input: {
