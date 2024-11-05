3109f227fa61624a4a334d6062b048e1a2465e1c
kazimorwan498 <kazimdmorwan498@gmail.com>
2024-11-05 16:25:50 +0600

    customize products-details page products-details section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     mode: "jit",
-    content: ["*"],
+    content: ["./index.html", "./src/pages/**/*.html"],
     theme: {
         extend: {
             colors: {
                 primary: "rgb(0,113,248)",
                 secondary: "rgb(238,98,107)",
-                secondaryHover: "rgb(207, 79, 86)",
-                secondaryActive: "rgb(170, 65, 71)",
                 white: "rgb(255, 255, 255)",
                 black: "rgb(30, 30, 30)",
+                lightGray: "rgb(232, 232, 232)",
             },
-        },
-        colors: {
-            primary: "rgb(0,113,248)",
-            secondary: "rgb(238,98,107)",
-            white: "rgb(255, 255, 255)",
-            black: "rgb(30, 30, 30)",
-            lightGray: "rgb(232, 232, 232)",
         },
     },
     plugins: [],
